d29049bfb299a26969fa5b514e9f0dde6685c777
Lowaax <florin.hamelin@gmail.com>
2026-04-17 10:48:33 +0200

    api suite

--- Backend/ysync/src/salles/salles.module.ts
+++ Backend/ysync/src/salles/salles.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SallesController } from './salles.controller';
 import { SallesService } from './salles.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [SallesController],
-  providers: [SallesService]
+  providers: [SallesService, PrismaService],
 })
 export class SallesModule {}
